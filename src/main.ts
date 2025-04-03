12b167d120c6306e1078c5e4836717a99e8eeb47
shailesh <smourya7425@gmail.com>
2025-04-03 15:19:02 +0530

    wip

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-
+  app.enableCors();
+  app.use(helmet());
   // ✅ Ensure ConfigService is returning Swagger config correctly
   const swaggerConfig = configService.get<any>('swagger');
   console.log('Swagger Config:', swaggerConfig); // Debugging
